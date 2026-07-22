fe4b001c5eceec9cf9d5fa0ee7b133ae85fb14a0
ayush0831 <vishwakarmaayush989@gmail.com>
2026-07-22 16:33:46 +0530

    Fix import statement for axios in Holdings.js

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios, from "axios";
+import axios from "axios";
 import { VerticalGraph } from "./VerticalGraph";
 
 // import { holdings } from "../data/data";
